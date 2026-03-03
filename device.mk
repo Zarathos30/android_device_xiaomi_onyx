b0cf223f8f7cdfa13a38e7ff6c34278f0889886b
LuK1337 <priv.luk@gmail.com>
2026-03-03 12:11:05 +0100

    onyx: Bump dEQP levels to 2024-03-01
    $ rg deqp.level vendor/etc/permissions
    vendor/etc/permissions/android.software.opengles.deqp.level.xml
    20:    <feature name="android.software.opengles.deqp.level" version="132645633" />
    
    vendor/etc/permissions/android.software.vulkan.deqp.level.xml
    20:    <feature name="android.software.vulkan.deqp.level" version="132645633" />
    $ python -q
    >>> hex(132645633)
    '0x7e80301'
    >>> 0x7e8, 0x03, 0x01
    (2024, 3, 1)
    
    Change-Id: I9ddcf2cd0c6a0ed253800f30176984440307bcd4

--- device.mk
+++ device.mk
@@ -187,11 +187,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_3.xml \
-    frameworks/native/data/etc/android.software.vulkan.deqp.level-2023-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2024-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
-    frameworks/native/data/etc/android.software.opengles.deqp.level-2023-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
+    frameworks/native/data/etc/android.software.opengles.deqp.level-2024-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
 
 PRODUCT_PACKAGES += \
     ANGLE
